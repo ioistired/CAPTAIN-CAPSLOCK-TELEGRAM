082d3c07b8a85d93340a9819c0891ae793c0f4ca
bmintz <bmintz@protonmail.com>
2018-06-18 13:28:58 -0500

    bot: begin transition to postgres

--- bot.py
+++ bot.py
@@ -2,11 +2,11 @@
 # encoding: utf-8
 
 import asyncio
-import json
-import random
 import sys
 from unicodedata import category as unicode_category
 
+import asyncpg
+from passlib.hash import argon2
 import discord
 
 
@@ -17,21 +17,16 @@ for c in map(chr, range(sys.maxunicode+1)):
 UPPERCASE_LETTERS = frozenset(UPPERCASE_LETTERS)
 del c
 client = discord.AutoShardedClient()
+hasher = argon2.using(parallelism=4, memory_cost=128*1024, rounds=20)
 
 @client.event
 async def on_ready():
-	load_shouts()
-	client.loop.create_task(save_shouts_loop())
 	print('Ready')
 
 @client.event
 async def on_message(message):
 	if not is_shout(message.content) or message.author.bot:
 		return
-
-	if shouts:
-		await message.channel.send(random.choice(shouts))
-	add_shout(message.content)
 
 def is_shout(str):
 	length = len(str)
@@ -43,36 +38,9 @@ def is_shout(str):
 			sum += 1
 	return (sum / length) > 0.5
 
-def add_shout(shout):
-	if shout not in shouts_set:
-		shouts.append(shout)
-	shouts_set.add(shout)
-
-def load_shouts():
-	global shouts, shouts_set
-	shouts = []
-	shouts_set = set()
-
-	try:
-		with open('shouts.txt') as f:
-			for shout in json.load(f):
-				add_shout(shout)
-	except FileNotFoundError:
-		pass
-
-def save_shouts():
-	with open('shouts.txt', 'w') as f:
-		json.dump(shouts, f, ensure_ascii=False, indent=0)
-
-async def save_shouts_loop():
-	while not client.is_closed():
-		save_shouts()
-		await asyncio.sleep(300)
 
 
 if __name__ == '__main__':
 	import os
 
 	client.run(os.environ['LOUDBOT_TOKEN'])
-	save_shouts()
-
