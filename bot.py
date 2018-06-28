e9a23b356843f62f61bf005d6b5831436238fd18
bmintz <bmintz@protonmail.com>
2018-06-28 12:05:37 -0500

    configure logging and stats

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
+import logging
 import json
 
 from discord.ext import commands
 
+logging.basicConfig()
+logger = logging.getLogger('bot')
+logger.setLevel(logging.INFO)
 
 class Loudbot(commands.AutoShardedBot):
 	def __init__(self, *args, **kwargs):
@@ -14,12 +18,13 @@ class Loudbot(commands.AutoShardedBot):
 		super().__init__(*args, command_prefix=commands.when_mentioned, **kwargs)
 
 	def run(self):
-		for extension in self.config['extensions']:
+		for extension in self.config['startup_extensions']:
 			self.load_extension(extension)
+			logger.info('loaded extension %s successfully', extension)
 		super().run(self.config['tokens']['discord'])
 
 	async def on_ready(self):
-		print('Ready')
+		logger.info('Ready')
 
 
 if __name__ == '__main__':
