a8f71a3b879060c363c067b1caccdc1ce26c8eed
Benjamin Mintz <bmintz@protonmail.com>
2019-08-13 17:04:56 +0000

    rewrite the shout detection
    importing the shout module is now 2× faster since it only parses the needed unicode properties needed,
    rather than parsing all of them and deleting unwanted ones

--- utils/shout.py
+++ utils/shout.py
@@ -21,26 +21,42 @@ def is_shout(str):
 
 	return length >= 4 and count / length > 0.5
 
+properties_path = Path(__file__).parent.parent / 'data' / 'DerivedCoreProperties.txt'
+
 def get_derived_core_properties():
 	properties = {}
 
-	with open(Path(__file__).parent.parent / 'data' / 'DerivedCoreProperties.txt') as f:
-		for line in map(str.strip, f):
-			if line.startswith('#') or not line:
-				continue
-
-			# ignore trailing comments too
-			line = ''.join(itertools.takewhile(lambda c: c != '#', line))
-
-			range, property = map(str.strip, line.split(';'))
-			range = unicode_range_to_range(range)
-
+	with open(properties_path) as f:
+		for property, range in parse_properties(f):
 			properties.setdefault(property, set()).update(map(chr, range))
 
 	return {property: frozenset(chars) for property, chars in properties.items()}
 
-def unicode_range_to_range(line):
-	range_str = line.split()[0]
+def get_derived_core_property(property):
+	chars = set()
+	desired = property
+
+	with open(properties_path) as f:
+		for property, range in parse_properties(f):
+			if property == desired:
+				chars.update(range)
+
+	return frozenset(chars)
+
+def parse_properties(f):
+	for line in map(str.strip, f):
+		if line.startswith('#') or not line:
+			continue
+
+		# ignore trailing comments too
+		line = ''.join(itertools.takewhile(lambda c: c != '#', line))
+
+		range, property = map(str.strip, line.split(';'))
+
+		range = unicode_range_to_range(range)
+		yield property, range
+
+def unicode_range_to_range(range_str):
 	return inclusive_range(*map(hex_to_int, range_str.split('..')))
 
 hex_to_int = functools.partial(int, base=16)
@@ -51,9 +67,7 @@ def inclusive_range(start, stop=None, step=1):
 	else:
 		stop += 1
 
-	return range(start, stop, step)
+	return range(start, start + 1 if stop is None else stop + 1, step)
 
 UPPERCASE_LETTERS = frozenset(filter(str.isupper, map(chr, range(maxunicode + 1))))
-DERIVED_CORE_PROPERTIES = get_derived_core_properties()
-DEFAULT_IGNORABLE = DERIVED_CORE_PROPERTIES['Default_Ignorable_Code_Point']
-del DERIVED_CORE_PROPERTIES  # comment if you need that, but we wanna save memory
+DEFAULT_IGNORABLE = get_derived_core_property('Default_Ignorable_Code_Point')
