4a16d541bd7dd99435dd970d5c9a601fccc20fa7
Benjamin Mintz <bmintz@protonmail.com>
2019-08-29 05:24:46 +0000

    more test cases

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import unicodedata
 from pathlib import Path
 
 from utils.shout import is_shout
@@ -10,8 +11,14 @@ assert not is_shout('\u200b' * 10)
 assert not is_shout('PR it')
 assert not is_shout('hello 10 GiB')
 assert not is_shout('I shall')
+assert not is_shout('Ok')
+assert not is_shout('OK')
 
-with open(Path(__file__).parent / 'shouts.txt') as f:
-	for line in map(str.strip, f):
-		if not line.startswith('#'):
-			assert is_shout(eval(line)), line
+assert is_shout('tfw MANUALLY_INITIATED_CRASH')
+assert is_shout(''.join(unicodedata.lookup('NEGATIVE SQUARED LATIN CAPITAL LETTER ' + c) for c in 'LONEBOY'))
+assert is_shout('I SHALL')
+assert is_shout('PR IT')
+assert is_shout('F U')
+assert is_shout('FU')
+assert is_shout('you went to college to be a WELL EDUCATED CITIZEN OF THE WORLD, nick')
+assert is_shout('🅱️🅱️🅱️')
