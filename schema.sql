8165629de19d32075ecdc3222a8a956d6c6c19b9
Io Mintz <io@mintz.cc>
2019-10-18 14:59:46 +0000

    schema: telegram IDs are 32-bit not 64-bit

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 SET TIME ZONE 'UTC';
 
 CREATE TABLE IF NOT EXISTS shout (
-	chat_id BIGINT NOT NULL,
-	message_id BIGINT NOT NULL,
+	chat_id INT4 NOT NULL,
+	message_id INT4 NOT NULL,
 	content TEXT NOT NULL,
 	time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
 
@@ -26,7 +26,7 @@ FOR EACH ROW EXECUTE PROCEDURE update_time_column();
 CREATE TYPE peer_type AS ENUM ('PeerChannel', 'PeerUser', 'PeerChat');
 
 CREATE TABLE IF NOT EXISTS opt (
-	id BIGINT NOT NULL,
+	id INT4 NOT NULL,
 	state BOOLEAN NOT NULL,
 	peer_type peer_type NOT NULL,
 
